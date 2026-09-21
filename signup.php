b2cf208d68827865565092d3c83f1adf5efec39c
Shreejal Shrestha <shreejalshrestha1@gmail.com>
2026-09-21 17:44:32 +0545

    Changes

--- signup.php
+++ signup.php
@@ -39,7 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         } else {
 
-            $error = "Something went wrong. Please try again.";
+            $error = "Please try again.";
         }
     }
 
